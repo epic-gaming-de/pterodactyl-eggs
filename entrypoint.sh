e9c6aba0edac5ea462967aaf4b8876f2f84cf943
Jakob Müller <j.mueller@outlook.de>
2018-01-06 13:41:08 +0100

    1.0

---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+sleep 2
+
+# Get Docker Variables
+SRCDS_APPID=`eval echo $(echo "$SRCDS_APPID")`
+HOME=/home/container
+
+"$HOME"/steamcmd/steamcmd.sh +login anonymous +force_install_dir "$HOME" +app_update "$SRCDS_APPID" +quit
+
+# Get all other Docker Variables
+GIT_DETAILS=`eval echo $(echo "$GIT_DETAILS")` #https://user:passwd@url
+GIT_BRANCHES=`eval echo $(echo "$GIT_BRANCHES")` #branch1,branch2,...
+GIT_BASE_BRANCH=`eval echo $(echo "$GIT_BASE_BRANCH")` #basebranch
+
+if [ ! -d ".git" ]; then
+	git clone "$GIT_DETAILS" "$GIT_BASE_BRANCH" . -q
+else
+	git fetch origin
+	git reset --hard
+fi
+
+IFS=',' read -ra ADDR <<< "$GIT_BRANCHES"
+for i in "${ADDR[@]}"; do
+	git merge origin/"$i" --allow-unrelated-histories
+done
+
+#git push origin "$GIT_BASE_BRANCH"
+
+cd "$HOME" || exit
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo "$STARTUP" | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+
+# Run the Server
+${MODIFIED_STARTUP}
